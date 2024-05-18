0391beac80efd675d8f6ac8aa9b4e22831e05f02
soledis_sacha_farinel <sacha.farinel@groupe-soledis.com>
2024-05-18 12:21:07 +0200

    test

--- src/Traits/ExportTranslationsTrait.php
+++ src/Traits/ExportTranslationsTrait.php
@@ -7,6 +7,6 @@ trait ExportTranslationsTrait
 {
     public function hookDisplayModuleConfigureExtraButtons(): string
     {
-        return $this->fetch("modules/{$this->name}/vendor/soledis/sldexporttranslations/views/templates/admin/configuration/hook/configuration-extra-buttons.tpl");
+        return $this->fetch("module:{$this->name}/vendor/soledis/sldexporttranslations/views/templates/admin/configuration/hook/configuration-extra-buttons.tpl");
     }
 }
